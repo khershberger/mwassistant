cbbf124582294c05e60460f610d01ca53ef29e4f
Kyle Hershberger <kyle.hershberger@gmail.com>
2018-01-26 11:04:30 -0700

    Rename conda package to mwassistant

--- conda-build/bld.bat
+++ conda-build/bld.bat
@@ -1,8 +1,8 @@
 if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\pysmith-menu-windows.json" "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\app.ico" "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\mwassistant-menu-windows.json" "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\mwassistant.ico" "%PREFIX%\Menu"
 
-copy "%RECIPE_DIR%\pysmith-app.py" "%SCRIPTS%"
+copy "%RECIPE_DIR%\mwassist-app.py" "%SCRIPTS%"
 
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
